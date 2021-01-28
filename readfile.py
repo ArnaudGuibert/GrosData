5e7c0f31aab2b1b3b9c9f2f5a58a83b396d75294
Arnaud <56339879+ArnaudGuibert@users.noreply.github.com>
2021-01-28 10:33:39 +0100

    séparation clean fonctions

--- readfile.py
+++ readfile.py
@@ -7,22 +7,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-""" Récupérer le corpus de textes """
-df = pd.read_json('data.json')
-df_reindexed = df.set_index('Id')
-
-
-""" Catégories par identifiant && Fusion dataframe """
-df_lbl = pd.read_csv("label.csv")
-df_reindexed['category'] = df_lbl['Category']
-
-
-""" Récupérer les catégories dans un dictionnaire (association Id / nom) """
-with open('categories_string.csv', mode = 'r') as infile:
-    reader = csv.reader(infile)
-    classes = { int(rows[1]) : rows[0] for rows in reader }
-
-
 def show_repartition(dataframe, classes):
     # Group by categories
     total = len(dataframe.index)
@@ -72,10 +56,31 @@ def show_repartition(dataframe, classes):
 
 def tf_idf_machine_learning(dataframe):
     # fonction pour le machine learning TF-IDF
+    X = dataframe['description']
+    y = dataframe['category']
+
+    # train / test / split data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 42, test_size = 0.3)
+
+    # declare & train TF-IDF vectorizer
     raise NotImplementedError
 
 
 if __name__ == "__main__":
+    # get text corpus
+    df = pd.read_json('data.json')
+    df_reindexed = df.set_index('Id')
+    
+    # get categories and merge dataframes
+    df_lbl = pd.read_csv("label.csv")
+    df_reindexed['category'] = df_lbl['Category']
+    
+    # get category names as dictionary
+    with open('categories_string.csv', mode = 'r') as infile:
+        reader = csv.reader(infile)
+        classes = { int(rows[1]) : rows[0] for rows in reader }
+    
+    # show info on data repartition
     show_repartition(df_reindexed, classes)
 
 
