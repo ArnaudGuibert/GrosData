a89763d62605d5254891ea231a8a28535858c60d
Reimu <remihuguenot89@gmail.com>
2021-02-05 11:03:30 +0100

    SSH CONNECTION FULL

--- SSHCONNECTION.py
+++ SSHCONNECTION.py
@@ -9,32 +9,42 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-import paraminko
+import paramiko
+import time
+import os
+from scp import SCPClient
 
-def connect(hostname, username, password):
+def connect(host,portNum , user, pswd):
     client = paramiko.SSHClient()
-    """ potentiellement besoin de chiffrer la clée
-    key = paramiko.RSAKey(data=base64.b64decode(b'AAA...'))
-    client.get_host_keys().add('ssh.example.com', 'ssh-rsa', key)
-    """
-
-    client.connect(hostname, username, password)
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+   
+    client.connect(host, portNum,user,pswd)
+    
     return client
 
 
-def getFileFromHadoop(client, path):
-    client.exec_command("fs -get "+path+" /HadoopDump/lastfile" )
-    sftp_client = client.open_sftp()
-    remote_file= sftp_client.open("/HadoopDump/lastfile")
-    return_file = remote_file.read()
-
-    remote_file.close()
-
-    client.exec_command("rm /HadoopDump/lastfile")
-    return return_file
+def getFileFromHadoop(client,filename,username,host,port):
+    client.exec_command("hadoop fs -get /user/data/"+filename+ " "+ filename)
+    scp = SCPClient(client.get_transport())
+    
+    scp.get( "/home/"+username+"/"+filename)
+    
+    client.exec_command("rm /home/"+username+"/"+filename)
+    
+    
+    
 def closeClient(client):
     client.close()
 
 
+def connTest(host,port,user,password,filename):
+    cli = connect(host,port,user,password)
+    getFileFromHadoop(cli,filename,user,host,port)
+    closeClient(cli)
+    
 
+
+
+connTest("localhost",2222,"maria_dev","maria_dev","data.json")
+print("THE END")
 
