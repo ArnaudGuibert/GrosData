d400f35ed56280b379cec3cccaea985da684e099
Arnaud <56339879+ArnaudGuibert@users.noreply.github.com>
2021-02-05 13:45:48 +0100

    nouveau preprocessing / creation save + load model

--- train_test.py
+++ train_test.py
@@ -1,35 +1,35 @@
+import csv
+import re
+
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-import time
-import csv
+import nltk
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 
-from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
 
 from joblib import dump, load
 
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem.snowball import SnowballStemmer
 
-# A faire (juste la première fois)
-# nltk.download('punkt')
-# nltk.download('stopwords')
-
-
-def pre_processing(dataframe):
+def pre_processing_old(dataframe):
     # preprocessing
     cachedStopWords = stopwords.words("english") + [ "," , "." , "'" ]
     stemmer = SnowballStemmer("english")
-    dataframe['description'] = dataframe['description'].apply(lambda x: pre_process_text(x, cachedStopWords, stemmer))
+    dataframe['description'] = dataframe['description'].apply(lambda x: pre_process_text_old(x, cachedStopWords, stemmer))
 
 
-def pre_process_text(text, cachedStopWords, stemmer):
+def pre_process_text_old(text, cachedStopWords, stemmer):
     # Remove stop words
     word_array = [ word for word in word_tokenize(text.lower()) if word not in cachedStopWords ]
     
@@ -40,6 +40,39 @@ def pre_process_text(text, cachedStopWords, stemmer):
     return ' '.join(word_array)
 
 
+def pre_processing(dataframe):
+    # preprocessing
+    stemmer = WordNetLemmatizer()
+    dataframe['description'] = dataframe['description'].apply(lambda x: pre_process_text(x, stemmer))
+
+
+def pre_process_text(document, stemmer):
+    # Remove all the special characters
+    document = re.sub(r'\W', ' ', document)
+    
+    # remove all single characters
+    document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document)
+    
+    # Remove single characters from the start
+    document = re.sub(r'\^[a-zA-Z]\s+', ' ', document) 
+    
+    # Substituting multiple spaces with single space
+    document = re.sub(r'\s+', ' ', document, flags=re.I)
+    
+    # Removing prefixed 'b'
+    document = re.sub(r'^b\s+', '', document)
+    
+    # Converting to Lowercase
+    document = document.lower()
+    
+    # Lemmatization
+    document = document.split()
+    document = [stemmer.lemmatize(word) for word in document]
+    
+    # Word array to text
+    return ' '.join(document)
+
+
 def show_repartition(dataframe, classes, title = 'Gender repartition by category'):
     # print title
     print(title + "\n")
@@ -47,7 +80,7 @@ def show_repartition(dataframe, classes, title = 'Gender repartition by category
     # Create dataframe grouped by category and gender
     grouped = dataframe.groupby(['category', 'gender']).size().unstack('gender')
     grouped['disparate_impact'] = grouped[['M', 'F']].max(axis='columns') / grouped[['M', 'F']].min(axis='columns')
-    grouped = grouped.rename(index = classes).sort_values('disparate_impact', ascending=False)
+    grouped = grouped.rename(index = classes)
 
     # Show dataframe
     print(grouped)
@@ -86,13 +119,27 @@ def tf_idf_machine_learning(dataframe, classes, withPreProcessing = False):
 
     # TF-IDF vectorizer + LinearSVC
     modelSVC = Pipeline([
-        ('tfidf', TfidfVectorizer()),
+        ('tfidf', TfidfVectorizer(stop_words = stopwords.words('english'))),
         ('clf', LinearSVC()),
     ])
 
     print("Start of model fitting...")
     modelSVC.fit(Xd_train, y_train)
     print("End of model fitting\n")
+
+    # name of config model file
+    if withPreProcessing:
+        name = 'clf_tfidf_prepro.joblib'
+    else:
+        name = 'clf_tfidf.joblib'
+
+    # save model
+    print("Saving model...")
+    dump(modelSVC, name)
+
+    # load model
+    print("Loading model...")
+    modelSVC = load(name)
 
     # make predictions on test data
     predictions = modelSVC.predict(Xd_test)
@@ -113,11 +160,10 @@ def tf_idf_machine_learning(dataframe, classes, withPreProcessing = False):
 
 
 if __name__ == "__main__":
-    # get text corpus
+    # get text corpus and merge dataframes
     df = pd.read_json('data.json')
     df_reindexed = df.set_index('Id')
-    
-    # get categories and merge dataframes
+
     df_lbl = pd.read_csv("label.csv")
     df_reindexed['category'] = df_lbl['Category']
     
@@ -126,14 +172,8 @@ if __name__ == "__main__":
         reader = csv.reader(infile)
         classes = { int(rows[1]) : rows[0] for rows in reader }
 
-    # show info on data repartition
-    tf_idf_machine_learning(df_reindexed, classes, False)
-
-
-"""
-dump(clf, 'filename.joblib')
-clf = load('filename.joblib')
-"""
+    # train model, show F1 / other scores
+    tf_idf_machine_learning(df_reindexed, classes, True)
 
 
 ### FIN DE SCRIPT ###
